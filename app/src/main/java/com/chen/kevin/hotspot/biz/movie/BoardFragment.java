a26830145865d93b8413dac5ccb64211bf9aafc4
kevin <987232210@qq.com>
2018-12-29 11:30:05 +0800

    榜单

--- app/src/main/java/com/chen/kevin/hotspot/biz/movie/BoardFragment.java
+++ app/src/main/java/com/chen/kevin/hotspot/biz/movie/BoardFragment.java
@@ -8,8 +8,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.chen.kevin.hotspot.R;
 import com.chen.kevin.hotspot.base.BaseFragment;
@@ -20,8 +18,6 @@ import com.chen.kevin.hotspot.base.BaseFragment;
 public class BoardFragment extends BaseFragment {
 
     public String[] tabTitle = {"电影", "读书"};
-    private ImageView ivBarLeft;
-    private TextView tvBarTitle;
 
 
     private TabLayout tlType;
@@ -47,12 +43,6 @@ public class BoardFragment extends BaseFragment {
 
     @Override
     protected void initView(View view) {
-        ivBarLeft = (ImageView) view.findViewById(R.id.iv_bar_left);
-        ivBarLeft.setVisibility(View.INVISIBLE);
-        tvBarTitle = (TextView) view.findViewById(R.id.tv_bar_title);
-        tvBarTitle.setText("榜单");
-
-
         tlType = (TabLayout) view.findViewById(R.id.tl_type);
 //        tlType.addTab(tlType.newTab().setText("电影"));
 //        tlType.addTab(tlType.newTab().setText("读书"));
